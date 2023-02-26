fb64cad2fbe482f1b1497de28473b64806adf2ef
brianhitchin <brianleehitchin@gmail.com>
2023-02-25 20:04:08 -0800

    group id bug fix v2

--- backend/db/migrations/6-create-event-image.js
+++ backend/db/migrations/6-create-event-image.js
@@ -24,7 +24,7 @@ module.exports = {
         allowNull: false
       },
       preview: {
-        type: Sequelize.STRING,
+        type: Sequelize.BOOLEAN,
         allowNull: false
       },
       createdAt: {
