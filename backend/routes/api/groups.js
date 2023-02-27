b0009f1f53ef5985514d7d728f86abf5d2025808
brianhitchin <brianleehitchin@gmail.com>
2023-02-27 07:51:05 -0800

    create group bug fix v1.6

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -152,7 +152,11 @@ router.post('/', requireAuth, async (req, res, next) => {
             city,
             state
         })
-        const checker = await Group.findOne({ where: { name: name, about: about, type: type, private: private }, order: [['createdAt', 'DESC']], limit: 1 })
+        const checker = await Group.findOne({ 
+            where: { name: name }, 
+            order: [['createdAt', 'DESC']], 
+            limit: 1 
+        })
         const newMembership = await Membership.create({
             userId: req.user.id,
             groupId: newGroup.id,
