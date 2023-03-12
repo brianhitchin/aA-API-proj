293260a024cb8d883155235a747a561584154426
brianhitchin <brianleehitchin@gmail.com>
2023-03-12 16:15:06 -0700

    form almost done

--- frontend/src/store/groups.js
+++ frontend/src/store/groups.js
@@ -4,6 +4,7 @@ const initialState = {}
 
 const POPULATE_PAGE = 'group/start'
 const GET_ONE_GROUP = 'group/getone'
+const ADD_GROUP = 'group/add'
 
 export const startGroups = (payload) => {
     return {
@@ -16,6 +17,28 @@ export const getGroup = (payload) => {
     return {
         type: GET_ONE_GROUP,
         payload
+    }
+}
+
+export const addGroup = (payload) => {
+    return {
+        type: ADD_GROUP,
+        payload
+    }
+}
+
+export const create = (value) => async dispatch => {
+    const response = await csrfFetch('api/groups', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(value)
+    })
+
+    if (response.ok) {
+        const data = await response.json()
+        dispatch(addGroup(data))
     }
 }
 
@@ -45,6 +68,10 @@ const groupsReducer = (state = initialState, action) => {
         case GET_ONE_GROUP:
             newState = {};
             newState = action.payload;
+            return newState;
+        case ADD_GROUP:
+            newState = {...state}
+            newState.groups[action.payload.id] = action.payload
             return newState;
         default:
             return state;
