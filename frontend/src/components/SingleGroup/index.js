a936d538b53ebc61edcd9352567b90a97d5fabca
brianhitchin <brianleehitchin@gmail.com>
2023-03-12 10:39:04 -0700

    format, fetching

--- frontend/src/components/SingleGroup/index.js
+++ frontend/src/components/SingleGroup/index.js
@@ -1,12 +1,42 @@
-import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, NavLink } from 'react-router-dom'
+import { useEffect } from 'react';
+import { oneGroup } from '../../store/groups';
 import './index.css'
 
 const SingleGroup = () => {
     const { groupId } = useParams();
+    const dispatch = useDispatch();
+    const group = useSelector(state => state.groups)
+
+    useEffect(() => {
+        dispatch(oneGroup(groupId))
+    }, [groupId, dispatch])
+
+    const organizer = group.Organizer
+    console.log('grp', group, group.Organizer)
+
     return (
         <div className="singlegroupmain">
-            <p1>Hello From Single Groups!</p1>
-            <p3>{groupId}</p3>
+            <div className='groupsum'>
+                <div className="groupnav">
+                    <NavLink to={'/groups'}>Groups</NavLink>
+                </div>
+                <div className="groupov">
+                    <div className="imgholder">
+                        <img src="http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/back05.jpg" 
+                        className="groupovimg" alt=""> 
+                        </img>
+                    </div>
+                    <div className="groupdt">
+                        <h1>{group.name}</h1>
+                        <h3 className="greyme">{`${group.city}, ${group.state}`}</h3>
+                        
+                        <h3 className="greyme">{group.private === true ? "Private" : "Public"}</h3>
+                        <button className="groupbutton" onClick={() => alert('Feature coming soon...')}>Join this group</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
