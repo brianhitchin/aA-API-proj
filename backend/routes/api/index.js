8101cf0ce7d8ca589f2c187c3a01c32d0971cc53
brianhitchin <brianleehitchin@gmail.com>
2023-06-08 20:17:47 -0700

    be membership

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,6 +6,7 @@ const venueRouter = require('./venues.js')
 const eventRouter = require('./events.js')
 const groupImageRouter = require('./group-images.js')
 const eventImageRouter = require('./event-images.js')
+const memberRouter = require('./membership.js')
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
@@ -17,5 +18,6 @@ router.use('/venues', venueRouter)
 router.use('/events', eventRouter)
 router.use('/group-images', groupImageRouter)
 router.use('/event-images', eventImageRouter)
+router.use('/membership', memberRouter)
 
 module.exports = router;
